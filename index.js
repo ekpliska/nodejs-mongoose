0551f4c1727a62c82dc4504bb7d67c6ce763cf4f
ekpliska <pliska.ek@gmail.com>
2020-03-23 21:31:26 +0300

    Initial project, added express, mongoose and express-handlebars

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+
+const appRouters = require('./routes');
+
+const PORT = 3000;
+
+const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(appRouters);
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/todos', {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+
+        app.listen(PORT, () => {
+            console.log('Server has been started');
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
